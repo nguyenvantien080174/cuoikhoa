819ee08b4dfd432395e40596fb50bde5ca7f5a76
nguyenvantien <nguyenvantien080174@gmail.com>
2024-04-14 00:17:46 +0700

    duong dan o local cua em khong co van de nhung khi push len git no xay ra loi kkhong mong muon. thuc su em da lam kip deadline nhung ket qua khong mong muon a :<<. Du commit nay co duoc nhan hay khong thi em van muon nop ket qua tot nhat ma em da lam duoc truoc deadline, em cam on a

--- js/delete-box.js
+++ js/delete-box.js
@@ -1,63 +1,63 @@
 
 
-const count = () => {
-  const countTodo = document.getElementById('count-childs-to-do');
-  const countDoing = document.getElementById('count-childs-Doing');
-  const countFinish = document.getElementById('count-childs-Finish');
-  const countBlocked = document.getElementById('count-childs-blocked');
+// const count = () => {
+//   const countTodo = document.getElementById('count-childs-to-do');
+//   const countDoing = document.getElementById('count-childs-Doing');
+//   const countFinish = document.getElementById('count-childs-Finish');
+//   const countBlocked = document.getElementById('count-childs-blocked');
 
-  const listToDo = document.getElementById('list-to-do').childElementCount;
-  const listDoing = document.getElementById('list-doing').childElementCount;
-  const listFinish = document.getElementById('list-finish').childElementCount;
-  const listblocked = document.getElementById('list-blocked').childElementCount;
+//   const listToDo = document.getElementById('list-to-do').childElementCount;
+//   const listDoing = document.getElementById('list-doing').childElementCount;
+//   const listFinish = document.getElementById('list-finish').childElementCount;
+//   const listblocked = document.getElementById('list-blocked').childElementCount;
 
-  countTodo.innerHTML = listToDo;
-  countDoing.innerHTML = listDoing;
-  countFinish.innerHTML = listFinish;
-  countBlocked.innerHTML = listblocked;
-}
+//   countTodo.innerHTML = listToDo;
+//   countDoing.innerHTML = listDoing;
+//   countFinish.innerHTML = listFinish;
+//   countBlocked.innerHTML = listblocked;
+// }
 
-const renderBox = () => {
-  const addlist = document.getElementById("list-to-do");
-  addlist.innerHTML = "";
-  // Lấy dữ liệu từ Local Storage và chuyển đổi thành mảng các đối tượng
-  const saveBoxs = JSON.parse(localStorage.getItem("key")) || [];
-  // Duyệt qua từng đối tượng trong mảng và hiển thị thông tin của nó
-  saveBoxs.forEach((task) => {
-    //tao box
-    addlist.insertAdjacentHTML(
-      "beforeend",
-      createBox(task.category, task.title, task.content, task.id, task.date)
-    );
-  });
+// const renderBox = () => {
+//   const addlist = document.getElementById("list-to-do");
+//   addlist.innerHTML = "";
+//   // Lấy dữ liệu từ Local Storage và chuyển đổi thành mảng các đối tượng
+//   const saveBoxs = JSON.parse(localStorage.getItem("key")) || [];
+//   // Duyệt qua từng đối tượng trong mảng và hiển thị thông tin của nó
+//   saveBoxs.forEach((task) => {
+//     //tao box
+//     addlist.insertAdjacentHTML(
+//       "beforeend",
+//       createBox(task.category, task.title, task.content, task.id, task.date)
+//     );
+//   });
 
-  const addlist2 = document.getElementById("list-doing");
-  addlist2.innerHTML = "";
-  // Lấy dữ liệu từ Local Storage và chuyển đổi thành mảng các đối tượng
-  const saveBoxs2 = JSON.parse(localStorage.getItem("doing")) || [];
-  // Duyệt qua từng đối tượng trong mảng và hiển thị thông tin của nó
-  saveBoxs2.forEach((task2) => {
-    //tao box
-    addlist2.insertAdjacentHTML(
-      "beforeend",
-      createBox(task2.category, task2.title, task2.content, task2.id, task2.date)
-    );
-  });
+//   const addlist2 = document.getElementById("list-doing");
+//   addlist2.innerHTML = "";
+//   // Lấy dữ liệu từ Local Storage và chuyển đổi thành mảng các đối tượng
+//   const saveBoxs2 = JSON.parse(localStorage.getItem("doing")) || [];
+//   // Duyệt qua từng đối tượng trong mảng và hiển thị thông tin của nó
+//   saveBoxs2.forEach((task2) => {
+//     //tao box
+//     addlist2.insertAdjacentHTML(
+//       "beforeend",
+//       createBox(task2.category, task2.title, task2.content, task2.id, task2.date)
+//     );
+//   });
 
-  const addlist3 = document.getElementById("list-finish");
-  addlist3.innerHTML = "";
-  // Lấy dữ liệu từ Local Storage và chuyển đổi thành mảng các đối tượng
-  const saveBoxs3 = JSON.parse(localStorage.getItem("finish")) || [];
-  // Duyệt qua từng đối tượng trong mảng và hiển thị thông tin của nó
-  saveBoxs3.forEach((task3) => {
-    //tao box
-    addlist3.insertAdjacentHTML(
-      "beforeend",
-      createBox(task3.category, task3.title, task3.content, task3.id, task3.date)
-    );
-  });
-  count();
-};
+//   const addlist3 = document.getElementById("list-finish");
+//   addlist3.innerHTML = "";
+//   // Lấy dữ liệu từ Local Storage và chuyển đổi thành mảng các đối tượng
+//   const saveBoxs3 = JSON.parse(localStorage.getItem("finish")) || [];
+//   // Duyệt qua từng đối tượng trong mảng và hiển thị thông tin của nó
+//   saveBoxs3.forEach((task3) => {
+//     //tao box
+//     addlist3.insertAdjacentHTML(
+//       "beforeend",
+//       createBox(task3.category, task3.title, task3.content, task3.id, task3.date)
+//     );
+//   });
+//   count();
+// };
 
 const createDelete = () => {
     for (let i = 0; i <= 50; i++) {
